bb63091dd6b1c84c35edf51e10b7549ab47b6cf6
Asus-32gb <gilad@pop-os.localdomain>
2023-06-22 22:48:45 +0300

    deploy plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,21 @@
 module.exports = ({ env }) => ({
-  //...
+  // ...
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+      sites: [
+        {
+          name: 'deluxe-concha-6c3afd',
+          id: "d27d56a1-a425-498e-b0eb-a12773f8cf62",
+          buildHook: "https://api.netlify.com/build_hooks/649206a6a0b3192336011408",
+          branch: 'main' // optional
+        }
+      ]
+    },
+  },
+  
+  // ...
   'import-export-entries': {
     enabled: true,
     config: {
@@ -22,4 +38,21 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
+  'update-static-content': {
+    enabled: true,
+    config: {
+      githubToken: env('GITHUB_TOKEN'), // accessing personal github token from env file
+      owner: 'giladx', // owner of the repo
+      repo: 'gatsby-spet', // name of the repo
+      workflowId: 'gatsby.yml', // workflowId OR filename
+      branch: 'main', // branch name
+      roles: ['strapi-super-admin'], // roles to access the plugin, by omitting roles any user can access the plugin
+    },
+  },
+ // ... 
+ "vercel-deploy": {
+    enabled: true,
+  },
+ // ...
+ 
 })
